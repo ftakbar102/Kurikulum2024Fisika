8e7c3de3657cc3952a1259db61da8366c869b9c2
ftakbar102 <ftakbar102@users.noreply.github.com>
2024-07-30 13:23:42 +0000

    deploy: f5b2e061449d7749dff8c2df38a7306bee5a3163

---
+++ service-worker-assets.js
@@ -0,0 +1,501 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-hDMiMML3RkLNSDdzRDDCbl5BSgoDV5T2+m4JVhdgmug=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-W0+p6qOob\/bzmFhQ+Fa2s5G0cod7G2PEowD3WOut9+0=",
+      "url": "css\/avenir-css\/avenir.css"
+    },
+    {
+      "hash": "sha256-1+t8gdpYvvBNfPJZES41PalW\/n3OlPZH04UuQTAHFDo=",
+      "url": "css\/avenir-css\/fonts\/AvenirNextLTPro-Cn_0.otf"
+    },
+    {
+      "hash": "sha256-SrKO3rmt6omLPp4haFigU43Bo8OgT6m\/WSE+gBwWOFM=",
+      "url": "css\/avenir-css\/fonts\/AvenirNextLTPro-Regular.ttf"
+    },
+    {
+      "hash": "sha256-W99ByXAHN+IOgIF1Vepn5gps\/gid29BOS8LyotzcJEw=",
+      "url": "css\/avenir-css\/fonts\/AvenirNextLTProDemi.ttf"
+    },
+    {
+      "hash": "sha256-z8OR40MowJ8GgK6P89Y+hiJK5+cclzFHzLhFQLL92bg=",
+      "url": "css\/bootstrap\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-gBwg2tmA0Ci2u54gMF1jNCVku6vznarkLS6D76htNNQ=",
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-eCHNZwNj082pg5bUR+5HJsvZZeDuKXyZ2kNY22rQdq8=",
+      "url": "css\/custom-fonts.css"
+    },
+    {
+      "hash": "sha256-gIolEiV6XGDVrWNGOmqSqK6+JIdX8N\/lpfRjQCf7wVY=",
+      "url": "css\/now-css\/fonts\/Now-Black.otf"
+    },
+    {
+      "hash": "sha256-4bKwIDd0z70+ck1EITUCueF1+YJB\/\/8SfoBnoS180gk=",
+      "url": "css\/now-css\/fonts\/Now-Bold.otf"
+    },
+    {
+      "hash": "sha256-V387Brm1hj1zI2SXLKRc3ISuI82TLT+VXT\/b75KCLSM=",
+      "url": "css\/now-css\/fonts\/Now-Light.otf"
+    },
+    {
+      "hash": "sha256-FJB0LZPTPrS1pGzPJBNLlGqkMjvczcEIHN2OWpzF4oY=",
+      "url": "css\/now-css\/fonts\/Now-Medium.otf"
+    },
+    {
+      "hash": "sha256-d4qsDQxQcdw9ZMJk2NT2soLyijB4KRglAOhQgVc0nPI=",
+      "url": "css\/now-css\/fonts\/Now-Regular.otf"
+    },
+    {
+      "hash": "sha256-0r\/tPqpQ\/FP5wYmmhcOzDfxF9Zb87sQge4XP1ptWjD0=",
+      "url": "css\/now-css\/fonts\/Now-Thin.otf"
+    },
+    {
+      "hash": "sha256-wAXadwLBQ0KcSXxnHYw8WbFFLNsIgY7x0EY9L2EaNrk=",
+      "url": "css\/now-css\/now.css"
+    },
+    {
+      "hash": "sha256-OzgaiKq8oV9g3dMx\/NgSIthgA4Mo6BjsZbjGl1kcz9c=",
+      "url": "datasource\/DoktorFisika\/DoktorFisikaBahanKajian.json"
+    },
+    {
+      "hash": "sha256-m0sgmItva3LggBx9s5uGUdKAw7XcWFxEiSCNv8mAw24=",
+      "url": "datasource\/DoktorFisika\/DoktorFisikaCPL.json"
+    },
+    {
+      "hash": "sha256-pS0ZwCQn5G\/gRbTKxfeZNEDi74Y7YJNRNW+EVVoRU1I=",
+      "url": "datasource\/DoktorFisika\/DoktorFisikaCPMK.json"
+    },
+    {
+      "hash": "sha256-MNsfYmHiwRRmhqOPrGf5DHYDajysYjUlScdzQStvr44=",
+      "url": "datasource\/DoktorFisika\/DoktorFisikaKatalog.json"
+    },
+    {
+      "hash": "sha256-pgkwIbKPLxK7xM5jAfeiD3n4yehgM6EhCsf8ddLbV9c=",
+      "url": "datasource\/DoktorFisika\/DoktorFisikaRelation.json"
+    },
+    {
+      "hash": "sha256-MS1VdFXRYAWZEoQyuCKYulmk6zRZ4iu+Bq7HOR8i4Hk=",
+      "url": "datasource\/DoktorNuklir\/DoktorNuklirBahanKajian.json"
+    },
+    {
+      "hash": "sha256-jIvfgbQpw5akq6uLq1b\/upJlY7WdOSAm0dAwXjvtzNY=",
+      "url": "datasource\/DoktorNuklir\/DoktorNuklirCPL.json"
+    },
+    {
+      "hash": "sha256-TyINQIA0Wmfovl9syUWslxGPHyf6OgAkuGXoOvcS4Nw=",
+      "url": "datasource\/DoktorNuklir\/DoktorNuklirCPMK.json"
+    },
+    {
+      "hash": "sha256-htA+syqawwQ1vawcQO7NCb38\/rpWtl3PnfWq6d\/dMAg=",
+      "url": "datasource\/DoktorNuklir\/DoktorNuklirKatalog.json"
+    },
+    {
+      "hash": "sha256-qTh8IBAOn\/RAOL6iyu4U6mrW9wiq7hkiw\/o6QQb9fys=",
+      "url": "datasource\/DoktorNuklir\/DoktorNuklirRelation.json"
+    },
+    {
+      "hash": "sha256-PYakB8WVo4xR+SkYqkRkJvAmQHUuuEZOZgGd3L4U3d8=",
+      "url": "datasource\/MagisterFisika\/MagisterFisikaBahanKajian.json"
+    },
+    {
+      "hash": "sha256-B+Ag9nLvozpTJi7T5XTHoFOPcPsCyPj8YUANcoBDd\/w=",
+      "url": "datasource\/MagisterFisika\/MagisterFisikaCPL.json"
+    },
+    {
+      "hash": "sha256-cWswMBaFTrUerUfqHi34hqLs6rkhqyC\/FlqvRD5lbr4=",
+      "url": "datasource\/MagisterFisika\/MagisterFisikaCPMK.json"
+    },
+    {
+      "hash": "sha256-KGgnMUasClzrhUMpp8DzSmLouQyFsemUCDskNjLLqvI=",
+      "url": "datasource\/MagisterFisika\/MagisterFisikaKatalog.json"
+    },
+    {
+      "hash": "sha256-kyWZ+I7FEq4yq6cGBKXGVUwZQy6hDTy8e1iBGIaGDC0=",
+      "url": "datasource\/MagisterFisika\/MagisterFisikaRelation.json"
+    },
+    {
+      "hash": "sha256-E2+L0YzGCPk0J22KzIkHO5nqYAyJsiB04vapMzmEFyI=",
+      "url": "datasource\/MagisterNuklir\/MagisterNuklirBahanKajian.json"
+    },
+    {
+      "hash": "sha256-WrTjR6fhjmQoQoztpQYpf2Ww+FH3Mcudbn8rfTxfCwE=",
+      "url": "datasource\/MagisterNuklir\/MagisterNuklirCPL.json"
+    },
+    {
+      "hash": "sha256-wfUxRPVoHDR4lN3bMbpBRoOx\/5pWCfGOoYb4bLuQ4q4=",
+      "url": "datasource\/MagisterNuklir\/MagisterNuklirCPMK.json"
+    },
+    {
+      "hash": "sha256-pkf+3Ha\/pz9r\/OsmTWgBUy74G2iBmoZc49yLlgN\/lkI=",
+      "url": "datasource\/MagisterNuklir\/MagisterNuklirKatalog.json"
+    },
+    {
+      "hash": "sha256-vBodFpu4aK\/PebkfaCGXreeyFiQvNt22gX3Mhe93XP0=",
+      "url": "datasource\/MagisterNuklir\/MagisterNuklirRelation.json"
+    },
+    {
+      "hash": "sha256-evzc\/uNQ373ZfQzU4diLgtBjde7jIVPVnvTmpskeYdg=",
+      "url": "datasource\/MagisterPengajaranFisika\/MagisterPengajaranFisikaBahanKajian.json"
+    },
+    {
+      "hash": "sha256-+Dp5R8M4thd50OGyRTF6BUbWLunnEEC2jDRSIOWfovA=",
+      "url": "datasource\/MagisterPengajaranFisika\/MagisterPengajaranFisikaCPL.json"
+    },
+    {
+      "hash": "sha256-mX27UBKPnjrbnBuZUc3r0ahSKJnVjARurzNbPwQJMmY=",
+      "url": "datasource\/MagisterPengajaranFisika\/MagisterPengajaranFisikaCPMK.json"
+    },
+    {
+      "hash": "sha256-u54XjeMOMdYRgli3yV2W\/xKX4tl6gKxQxsq9HU2sNd8=",
+      "url": "datasource\/MagisterPengajaranFisika\/MagisterPengajaranFisikaKatalog.json"
+    },
+    {
+      "hash": "sha256-kMSLxBxn7sJDS5IXxOF\/t7Ct2jgHyvVMKDrxv+NzkkA=",
+      "url": "datasource\/MagisterPengajaranFisika\/MagisterPengajaranFisikaRelation.json"
+    },
+    {
+      "hash": "sha256-L5Z7yYHU4KS8mbi26GAVt6Jx4WVfTjG+2Ml3tVMV6yA=",
+      "url": "datasource\/MagisterSainsKomputasi\/MagisterSainsKomputasiBahanKajian.json"
+    },
+    {
+      "hash": "sha256-QmMLvdH2CooOH5n9a\/jPyCQMKduNqeuhw9uiEq2eUdU=",
+      "url": "datasource\/MagisterSainsKomputasi\/MagisterSainsKomputasiCPL.json"
+    },
+    {
+      "hash": "sha256-ERxyUG7B6wXNRZviRAwKSotGgkj8MbbG2wkkddYlI48=",
+      "url": "datasource\/MagisterSainsKomputasi\/MagisterSainsKomputasiCPMK.json"
+    },
+    {
+      "hash": "sha256-DMPFedPgD4k95TlwoSM+3eI336ELpds7\/UUygC9Bo5g=",
+      "url": "datasource\/MagisterSainsKomputasi\/MagisterSainsKomputasiKatalog.json"
+    },
+    {
+      "hash": "sha256-qTh8IBAOn\/RAOL6iyu4U6mrW9wiq7hkiw\/o6QQb9fys=",
+      "url": "datasource\/MagisterSainsKomputasi\/MagisterSainsKomputasiRelation.json"
+    },
+    {
+      "hash": "sha256-tA3lnddrAjs0UMoRSI2Lk84ohtcZ2rYoQs9wNt8Jh8E=",
+      "url": "datasource\/SarjanaFisika\/SarjanaFisikaBahanKajian.json"
+    },
+    {
+      "hash": "sha256-euhbJ+eCIBohqxuhsvrTtF8yflOLci49pLkhPXtX4rw=",
+      "url": "datasource\/SarjanaFisika\/SarjanaFisikaCPL.json"
+    },
+    {
+      "hash": "sha256-PYtuI8OCgpj9tvShT8VDKxNOXf5jTJut22aFeXTGazs=",
+      "url": "datasource\/SarjanaFisika\/SarjanaFisikaCPMK.json"
+    },
+    {
+      "hash": "sha256-1KqZkHpFqxmH2MqXPtT5JCFjGxQOBWET3drHQmYc8YU=",
+      "url": "datasource\/SarjanaFisika\/SarjanaFisikaKatalog.json"
+    },
+    {
+      "hash": "sha256-1qEX4RUPNpUOJvgTItPC1bggKbiphdTrAfNG1zGduU4=",
+      "url": "datasource\/SarjanaFisika\/SarjanaFisikaRelation.json"
+    },
+    {
+      "hash": "sha256-uflLtxRWGMI0P34D3yZCaM0SLkDizo24pTUQxGwT\/lo=",
+      "url": "favicon.ico"
+    },
+    {
+      "hash": "sha256-X\/2Jut2KYAggwKfZd8Hw9X4xmCGUvwuWg57xAonSkRM=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-X\/2Jut2KYAggwKfZd8Hw9X4xmCGUvwuWg57xAonSkRM=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-TzUDiQi8eeb8FvOBTw\/x9B49zMGqqDRsS\/q47qO+H8E=",
+      "url": "img\/FisikaLogoHorizontal.png"
+    },
+    {
+      "hash": "sha256-U\/l5OqMYHtsbR4rOvj69+Uxkdkl4SeEft8lxgiEcGV8=",
+      "url": "img\/FisikaLogoText.png"
+    },
+    {
+      "hash": "sha256-YFh49wNYfCi2wgv1C86Q7z1SsOwSWn\/LeBJVkpekSog=",
+      "url": "img\/FisikaLogoVertical.png"
+    },
+    {
+      "hash": "sha256-OBLtZ\/TDcIbJl6ADITT8fFh9HDoCWz3baI0RUM1BVSY=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-ZLIUU7n3MNoinJ6BiRaH6sUSZkCUz10DP41m0Pg8SXo=",
+      "url": "manifest.webmanifest"
+    },
+    {
+      "hash": "sha256-paIeuWnSzjeLGITWkk4p3rcejUsZWCJGPeOK9UhWXtg=",
+      "url": "sample-data\/weather.json"
+    },
+    {
+      "hash": "sha256-gvHfnndfEu1tRf0rFb5988rWq7ITIotOaE8+AMbKYbc=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-lHMDTsz7cQ0CIanynbga9kWuwz7B4eQoz9mX3mlfkIY=",
+      "url": "_framework\/dotnet.js"
+    },
+    {
+      "hash": "sha256-soZyw6XWXOLKhA26AOyzSpCP5cAsw1wvhdPHSYNTMfQ=",
+      "url": "_framework\/dotnet.runtime.8.0.7.3dp1i72u0p.js"
+    },
+    {
+      "hash": "sha256-qPzRJ7vpZXzgfn\/zRN4sE1DCkIMOhv0uhpIe\/6UMYj4=",
+      "url": "_framework\/icudt_CJK.dat.br"
+    },
+    {
+      "hash": "sha256-8C0k+ite5nej4L1bjieJp6Wsf9DY82n6f85vCedJK\/Y=",
+      "url": "_framework\/icudt_EFIGS.dat.br"
+    },
+    {
+      "hash": "sha256-fv9mQqIBur\/JQyexfKrVYjdpw7zdaW4zV4qxIkzgxlI=",
+      "url": "_framework\/icudt_no_CJK.dat.br"
+    },
+    {
+      "hash": "sha256-AbVgOBYKExaRXa8+4YvxYMVfvNiwnn+Q1xPVoCuZzRE=",
+      "url": "_framework\/blazor.boot.json.br"
+    },
+    {
+      "hash": "sha256-7mUf77X9kA8XQSanKrMFFP42Cd+qNdOCGHeI1c5JxXo=",
+      "url": "_framework\/dotnet.native.8.0.7.nwfmiaueox.js"
+    },
+    {
+      "hash": "sha256-MG9TwgAJ4j8Ctw7ARaEgKCha+ORFl\/LU4cTloZR2x04=",
+      "url": "_framework\/dotnet.native.wasm.br"
+    },
+    {
+      "hash": "sha256-jb7udHLMk5woAZq1aotd8vLDU4W3E6HvPL+CROjFBaU=",
+      "url": "_framework\/Blazor-ApexCharts.wasm.br"
+    },
+    {
+      "hash": "sha256-LB\/tY\/KBcVQsM3OYW16ClW3R2xgKFgeV3rlOWm2GZpE=",
+      "url": "_framework\/Blazored.SessionStorage.wasm.br"
+    },
+    {
+      "hash": "sha256-+UgWtvnTKdCfpaIA52kcxAEGxQwPNBITDXfB9kjX5sU=",
+      "url": "_framework\/KurikulumPascasarjanaFisika.wasm.br"
+    },
+    {
+      "hash": "sha256-u6SyRuEWxmdqc+q3n7CeBH3gGDUXkmFB02FNBie04ME=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Forms.wasm.br"
+    },
+    {
+      "hash": "sha256-AY1ZzCEOErTdLt9hNRZeJnrnG1u5x0Rh3cDRfU\/CW7w=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.wasm.br"
+    },
+    {
+      "hash": "sha256-jmZjCw7r1GBHhcmc6yqVkOp\/c18fLjL5oxAxWhXAnJY=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Web.wasm.br"
+    },
+    {
+      "hash": "sha256-30NYFCv+CRS6LmouZXQJpBrMa3o3Spe58\/\/q0A8nCWI=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.WebAssembly.wasm.br"
+    },
+    {
+      "hash": "sha256-qUtVJGIGF4M2YauSy3psGjD0LAbqEwEXwEGtXmzP6ik=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Abstractions.wasm.br"
+    },
+    {
+      "hash": "sha256-puc0WP+4cA3bIzF7\/dvV+rhyAlrFTnQUNfFAN535+nw=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Json.wasm.br"
+    },
+    {
+      "hash": "sha256-U9KRKAdCENZDTbfEfkacBOt8wWtDZ6IRMpPayshGTS4=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.wasm.br"
+    },
+    {
+      "hash": "sha256-psFQxXO6JBbTUbpAZ62m4BqTKk6H9SjhDB0JHO9MrG4=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.wasm.br"
+    },
+    {
+      "hash": "sha256-yM5JCjHMH1LKN\/bAbi\/lvPz0d+DUiKnHW+q7y4xA1DM=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.wasm.br"
+    },
+    {
+      "hash": "sha256-caxLGG5bxunq2clrIGbMlhtyJc6OC6UVXWCFRFfdMN4=",
+      "url": "_framework\/Microsoft.Extensions.Localization.Abstractions.wasm.br"
+    },
+    {
+      "hash": "sha256-vH45y4j0AOmDYUgiQXh3TJhipfVQ9v1iu4hOf2KF98o=",
+      "url": "_framework\/Microsoft.Extensions.Localization.wasm.br"
+    },
+    {
+      "hash": "sha256-MY5ZOQAObN9vkuUnOndI9RiEGcLm81EmvHG6WHZbq1A=",
+      "url": "_framework\/Microsoft.Extensions.Logging.Abstractions.wasm.br"
+    },
+    {
+      "hash": "sha256-sjxlChh0Ip3+nIoCrfPJS1D0JXHJvocIToj0DZAwS2Q=",
+      "url": "_framework\/Microsoft.Extensions.Logging.wasm.br"
+    },
+    {
+      "hash": "sha256-DTL3EIb3vuG6z3Usjv4ayfYPtjeMU5\/q0BC4lOLSIkU=",
+      "url": "_framework\/Microsoft.Extensions.Options.wasm.br"
+    },
+    {
+      "hash": "sha256-LR6aVMK9dzfWsqlzsMDBE578qZg2IAWjjf9WWf4llXE=",
+      "url": "_framework\/Microsoft.Extensions.Primitives.wasm.br"
+    },
+    {
+      "hash": "sha256-Y4V4y6JkJKKa9Zd6TAj+yCdXQW0QWXybW1PrgqDslIM=",
+      "url": "_framework\/Microsoft.JSInterop.wasm.br"
+    },
+    {
+      "hash": "sha256-fWJt4CB6ciQzrvPYF+vfVZAUhw6JW\/PTG8mLKATi5Fw=",
+      "url": "_framework\/Microsoft.JSInterop.WebAssembly.wasm.br"
+    },
+    {
+      "hash": "sha256-uGhacl1vrpHnlu4snOonmWMc2SLYzpGAUgkeuOOBKzg=",
+      "url": "_framework\/MudBlazor.wasm.br"
+    },
+    {
+      "hash": "sha256-rO8t3X9DH2ejNmx8IHleePHghyyVPnvOfHpjgVCHS4s=",
+      "url": "_framework\/System.Collections.Concurrent.wasm.br"
+    },
+    {
+      "hash": "sha256-zcWpFwH0E4gRgzHAEpRkQ8hMUuehUL3berDz0fQfOwQ=",
+      "url": "_framework\/System.Collections.Immutable.wasm.br"
+    },
+    {
+      "hash": "sha256-eaNcsGR1IJ5bt0wdHQO8I6o9Ev5gZo5plf7upbxKBJ8=",
+      "url": "_framework\/System.Collections.wasm.br"
+    },
+    {
+      "hash": "sha256-qjEDDNUsHxm0QSPvaIlTxHM7YZ1v+0oRP1Z8bgg2mq8=",
+      "url": "_framework\/System.ComponentModel.Annotations.wasm.br"
+    },
+    {
+      "hash": "sha256-kadW\/Gq6Jtqm+jBfraPkDLtV2xAl0Y66X0VOUQOlHfs=",
+      "url": "_framework\/System.ComponentModel.Primitives.wasm.br"
+    },
+    {
+      "hash": "sha256-EhbxTeFuCr7Mf7kxnuPLpDd3IVoyUEaVW89+ZuwPnzc=",
+      "url": "_framework\/System.ComponentModel.TypeConverter.wasm.br"
+    },
+    {
+      "hash": "sha256-ItJcXjW6MjHEy0eVrJ4FFdUzI08CLb2h\/4NH5jCMe2U=",
+      "url": "_framework\/System.ComponentModel.wasm.br"
+    },
+    {
+      "hash": "sha256-p8HgKTrqUZilS+yxlLPOfvJLVoXGoHYKTPkJtZcAg30=",
+      "url": "_framework\/System.Console.wasm.br"
+    },
+    {
+      "hash": "sha256-o+HkBkEkGLIWvQuN9BYsxBvNc1fkBhopjtFP7qZuFwM=",
+      "url": "_framework\/System.Diagnostics.DiagnosticSource.wasm.br"
+    },
+    {
+      "hash": "sha256-+sMRtr6GmSjPZXDT8XXIWYlHPdk85TNX772eVvwIUTk=",
+      "url": "_framework\/System.Linq.Expressions.wasm.br"
+    },
+    {
+      "hash": "sha256-yuloB45GBI82OVyFOXBQp5s7VCSMqnJW0JAdkEe1oUw=",
+      "url": "_framework\/System.Linq.Queryable.wasm.br"
+    },
+    {
+      "hash": "sha256-rOgPGAKIFkuQ3rz3XNJJ8GAdDm5W8zcta5LQTuzfIgU=",
+      "url": "_framework\/System.Linq.wasm.br"
+    },
+    {
+      "hash": "sha256-WlBAjwVJ170AqrL4hEbeAV\/z0c1SD\/5dxQkbMCLXrwQ=",
+      "url": "_framework\/System.Memory.wasm.br"
+    },
+    {
+      "hash": "sha256-1RgR52kHe\/1t9VQO6bNC8qz23HonTc3Pdd5glgi+lLE=",
+      "url": "_framework\/System.Net.Http.Json.wasm.br"
+    },
+    {
+      "hash": "sha256-sAlu238GafhcHyg4Uz0NVSxtZQJAQWMsVTbvu3T1SRM=",
+      "url": "_framework\/System.Net.Http.wasm.br"
+    },
+    {
+      "hash": "sha256-onS7sNGvayt17vrT4B32haKYfKLhNB6CvzIb8XADU9c=",
+      "url": "_framework\/System.Net.Primitives.wasm.br"
+    },
+    {
+      "hash": "sha256-5vQ+QjneqMTn\/TPqhOe8YXfmbJjsxCfPobwYQTl8FoY=",
+      "url": "_framework\/System.ObjectModel.wasm.br"
+    },
+    {
+      "hash": "sha256-JZZsev+PuHn0nyxaNPHDsdIdx7wlTBKTYf4+vu9b090=",
+      "url": "_framework\/System.Private.CoreLib.wasm.br"
+    },
+    {
+      "hash": "sha256-n5Nbk3X0oI5KeORW4iGfCIV3orPAYbuYPW0Gs+yXYsQ=",
+      "url": "_framework\/System.Private.Uri.wasm.br"
+    },
+    {
+      "hash": "sha256-dpVxrs8DoUQBUyyUrWSBkU7J\/9nE6sLm3PQUfLjUzWg=",
+      "url": "_framework\/System.Runtime.InteropServices.JavaScript.wasm.br"
+    },
+    {
+      "hash": "sha256-P0\/FbKFuYTdRdIdCODgIRJdj+d9KbeFzmBmbwzvvsq8=",
+      "url": "_framework\/System.Runtime.Serialization.Primitives.wasm.br"
+    },
+    {
+      "hash": "sha256-sZjxIcneh90wPlBsIpiPmGEqFRDCseexTTHbOxj3wZY=",
+      "url": "_framework\/System.Runtime.wasm.br"
+    },
+    {
+      "hash": "sha256-qf1Hl8I9X7OzTYQ0hKWGq9zBNIo20iziKOZjDkeunEY=",
+      "url": "_framework\/System.Text.Encodings.Web.wasm.br"
+    },
+    {
+      "hash": "sha256-49VY8+1G7C3mVMQxUQtX4vfGC79eWfmnR++A7ZDNUq0=",
+      "url": "_framework\/System.Text.Json.wasm.br"
+    },
+    {
+      "hash": "sha256-s\/4ZNFnJYlIiCRAerj4o6QaoNHZdDpRSZ3xgz+LAr5M=",
+      "url": "_framework\/System.Text.RegularExpressions.wasm.br"
+    },
+    {
+      "hash": "sha256-v9VDA5\/D4p4jylhjq7hFgxYNDMAUg01CfYXK0RQUaak=",
+      "url": "_framework\/System.Threading.wasm.br"
+    },
+    {
+      "hash": "sha256-amUA1RW9OEwvGKIe2ZeWmz2hvPsyUN5R2zd1BhAsU4M=",
+      "url": "_framework\/System.wasm.br"
+    },
+    {
+      "hash": "sha256-aoJspHsCLCersv5gImdomKyzg0wkQSuYDjd17RjwbCk=",
+      "url": "KurikulumPascasarjanaFisika.styles.css"
+    },
+    {
+      "hash": "sha256-We76yQ6BbUqy3OL7pB4AiChSOtAH\/BdDsZ0Z+MzXvD0=",
+      "url": "_content\/Blazor-ApexCharts\/css\/apexcharts.css"
+    },
+    {
+      "hash": "sha256-uwIUbwxCZoqz\/Kjbogiig2u2k+0WhtT59nSTwFkacOo=",
+      "url": "_content\/Blazor-ApexCharts\/js\/apexcharts.esm.js"
+    },
+    {
+      "hash": "sha256-2d9DV\/4aid1fftymTiSC7nxGkJlagyofi4dawOjgzgo=",
+      "url": "_content\/Blazor-ApexCharts\/js\/blazor-apexcharts.js"
+    },
+    {
+      "hash": "sha256-u6fbmTLmtsB+HgoD7TaQR+HK2eLlKuUV3klmACbUZZQ=",
+      "url": "_content\/MudBlazor\/MudBlazor.min.css"
+    },
+    {
+      "hash": "sha256-Kpp99KQmZRTnEUw6ARiwaFvMbeAmV2FlZSLsPfInr88=",
+      "url": "_content\/MudBlazor\/MudBlazor.min.js"
+    },
+    {
+      "hash": "sha256-wX1LB4yuBRFM4fmdSsH6jBlCch9uCHXV9uFTxnz2blQ=",
+      "url": "decode.min.js"
+    },
+    {
+      "hash": "sha256-TBOzJXpz1bu73tg17FiEOqiO8bQR5aFQdjA1fZSl5JA=",
+      "url": "brotliloader.min.js"
+    }
+  ],
+  "version": "RJonI9\/q"
+};
